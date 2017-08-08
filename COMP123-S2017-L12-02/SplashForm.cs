941a2f2682ee0ec919a9df73648cf46fc69dbdaa
Angelaaa <Angelaaa@LAPTOP-0Q8KPPTN>
2017-08-08 16:45:15 -0400

    Create the SplashForm

---
+++ COMP123-S2017-L12-02/SplashForm.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+/*
+ * Name:Yanying Zhan
+ * Date: Aug 8
+ * Description: SplashForm
+ * Version: 0.1 - Create the SplashForm
+ */
+namespace COMP123_S2017_L12_02
+{
+    public partial class SplashForm : Form
+    {
+        public SplashForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
